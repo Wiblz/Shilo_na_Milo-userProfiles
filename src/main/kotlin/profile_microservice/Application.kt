5023c428b0d1b9c873bd26dda7339eae973fe6ab
Wiblz <Wiblzcode@gmail.com>
2018-12-02 17:52:54 +0200

    Add AllowOrigin parameter

--- src/main/kotlin/profile_microservice/Application.kt
+++ src/main/kotlin/profile_microservice/Application.kt
@@ -2,9 +2,23 @@ package profile_microservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
 
 @SpringBootApplication
-open class Application
+open class Application {
+    @Configuration
+    @EnableWebFlux
+    open class WebConfig: WebFluxConfigurer {
+        override fun addCorsMappings(registry: CorsRegistry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("*")
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
